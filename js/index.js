7237d72502587dffdfe5c5beaaa7c27dafc1dbca
Kim-hyosun <jirehmuze@gmail.com>
2022-12-22 15:45:10 +0900

    merge_v1

--- js/index.js
+++ js/index.js
@@ -42,4 +42,56 @@ art.forEach((el) => {artObserver.observe(el);})
 }())
 
 
+//
+
+  // 마우스 커서(반전 원/근데 한 페이지에서만 보임.. 왜?ㅠㅠ)
+  
+const circle = document.querySelector('.circle');
+let mouseX = 0;
+let mouseY = 0;
+let circleX = 0;
+let circleY = 0;
+let speed = 0.4;
+const animate = () => {
+  let distX = mouseX - circleX;
+  let distY = mouseY - circleY;
+  let translatedValue = `translate3d(${circleX}px, ${circleY}px, 0 )`;
+
+  circleX = circleX + distX * speed;
+  circleY = circleY + distY * speed;
+  circle.style.transform = translatedValue;
+
+  requestAnimationFrame(animate);
+};
+animate();
+window.addEventListener('mousemove', e => {
+  mouseX = e.clientX - circle.offsetWidth / 4;
+  mouseY = e.clientY - circle.offsetHeight / 4;
+});
+
+
+// 마우스커서 안 보이게 (수정해야 함...)
+// window.document.body.style.cursor = 'none';
+
+//가로스크롤
+(function(){
+const container = document.querySelector('.container');
+const stickyBox = document.querySelector('.sticky_box');
+const horizontal = document.querySelector('.horizontal');
+
+function calcHt(ht) {
+  return ht.scrollWidth - document.documentElement.clientWidth + document.documentElement.clientHeight;
+}
+
+container.style.height = `${calcHt(horizontal)}px`;
+
+addEventListener('scroll', scrolled);
+
+function scrolled() {
+  horizontal.style.transform = `translate(-${stickyBox.offsetTop}px)`;
+}
+}())
+
+  
+
 
