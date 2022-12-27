15b301c8a6cbbdf5d1baf474e4c43ce0c51a1e22
Kim-hyosun <113571272+Kim-hyosun@users.noreply.github.com>
2022-12-27 13:26:34 +0900

    Update index.js
    mainjs

--- js/index.js
+++ js/index.js
@@ -133,7 +133,7 @@ const stickyBox = document.querySelector('.sticky_box');
 const horizontal = document.querySelector('.horizontal');
 
 function calcHt(ht) {
-  return ht.scrollWidth - document.documentElement.clientWidth + document.documentElement.clientHeight + (document.documentElement.clientWidth / 100 * 10) - (document.documentElement.clientHeight);
+  return ht.scrollWidth - document.documentElement.clientWidth + document.documentElement.clientHeight + 200;
 }
 container.style.height = `${calcHt(horizontal)}px`;
 // console.log(container.getBoundingClientRect().top)
