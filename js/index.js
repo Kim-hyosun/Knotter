162846a07703e77bfb2b440778d80657a58b1c48
Kim-hyosun <113571272+Kim-hyosun@users.noreply.github.com>
2022-12-28 00:49:06 +0900

    main_css_animation(-)JS_value(+)

--- js/index.js
+++ js/index.js
@@ -133,7 +133,8 @@ const stickyBox = document.querySelector('.sticky_box');
 const horizontal = document.querySelector('.horizontal');
 
 function calcHt(ht) {
-  return ht.scrollWidth - document.documentElement.clientWidth + document.documentElement.clientHeight + (document.documentElement.clientWidth / 100 * 10) - (document.documentElement.clientHeight) ;
+  return ht.scrollWidth - document.documentElement.clientWidth + document.documentElement.clientHeight + 200;
+   //(document.documentElement.clientWidth / 100 * 10) - (document.documentElement.clientHeight) ;
 }
 container.style.height = `${calcHt(horizontal)}px`;
 // console.log(container.getBoundingClientRect().top)
